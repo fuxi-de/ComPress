490e3fe57bfd309cce508656947d0baa4f8a9439
Schildwächter <florian.schildwaecht@hbv83033757.bauer-de.bauermedia.group>
2018-10-02 17:19:38 +0200

    fixed twig error // base.twig must define content block to be overridden in extending files

--- public/content/themes/your_theme/templates/modules/quote/quote.php
+++ public/content/themes/your_theme/templates/modules/quote/quote.php
@@ -3,17 +3,17 @@ class Quote_Element extends Layotter_Element
 {
     protected function attributes() {
         $this->title       = 'Quote';
-        $this->description = 'Quote Element mit einem Kreis als Hintergrund';
+        $this->description = 'Quote Element';
         $this->icon        = 'font';
-        $this->field_group = 'group_5b96829569b7d';
+        $this->field_group = 'acf_quote';
     }
 
     protected function frontend_view($fields) {
-      Timber::render('quote.twig', $fields);
+        Timber::render('quote.twig', $fields);
     }
 
     protected function backend_view($fields) {
-      Timber::render('quote.twig', $fields);
+        Timber::render('quote.twig', $fields);
     }
 }
 
